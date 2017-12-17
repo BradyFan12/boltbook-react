ad309e7c6222dd336edab6d9e0d1e11bec9d8fca
Clayton Ray <iamclaytonray@gmail.com>
2017-12-16 21:48:11 -0500

    Add LoggedInUser query

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './main.css';
+import { CurrentUser } from 'containers/LoggedInUser';
+import { Loading } from 'shared/Loading';
+import { ErrorCard } from 'shared/ErrorCard';
 
 export const Nav = () => {
   return (
@@ -8,14 +11,24 @@ export const Nav = () => {
       <div className='searchbar'>
         <input type='text' className='form-control' placeholder='Search' />
       </div>
-      <div className='links'>
-        <NavLink to='/iamclaytonray'>Clayton</NavLink> {/* Should be ${username} - need props */}
-        <NavLink to='/feed'>Home</NavLink>
-        <NavLink to='/find-friends'>Find Friends</NavLink>
-        <i className='fa fa-users' aria-hidden='true'></i>
-        <i className='fa fa-comment' aria-hidden='true'></i>
-        <i className='fa fa-bell' aria-hidden='true'></i>
-      </div>
+      <CurrentUser
+        render={(loading, error, loggedInUser) => {
+          
+          if (loading) { return <Loading />; }
+          if (error) { return <ErrorCard />; }
+
+          return (
+            <div className='links'>
+              <NavLink to={`${loggedInUser.username}`}>{loggedInUser.firstName}</NavLink>
+              <NavLink to='/feed'>Home</NavLink>
+              <NavLink to='/find-friends'>Find Friends</NavLink>
+              <i className='fa fa-users' aria-hidden='true'></i>
+              <i className='fa fa-comment' aria-hidden='true'></i>
+              <i className='fa fa-bell' aria-hidden='true'></i>
+            </div>
+          )
+        }}
+      />
     </div>
   );
 }
